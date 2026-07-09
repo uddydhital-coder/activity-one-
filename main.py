9c7673715c9b06b7fd6b4ac9e6e2b6ec1c22e0f8
uddydhital-coder <uddydhital@gmail.com>
2026-07-09 17:50:09 +1000

    Update main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#Printing a greeting message
+print("Hello, world!")
+
+#Printing my name
+print("My name is Alex.")
+#Practicing a fun fact about myself
+# print ("I love plaing soccer")
+print(" I love playing soccer")
+
+#perfoming a simple calculation
+print("The result of 3 + 5 is", 3+5)
